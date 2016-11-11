ffd739f0b59fd8e4fb554d7f98ec39280c43e8f8
Code-Dude <smythe3000>
2016-11-10 16:33:21 -0800

    second commit, covers through lessons 1 - 3.

---
+++ fundraiser/fundraiser.rb
@@ -0,0 +1,11 @@
+fundraiser1 = "ABC"
+fundraiser2 = "DEF"
+fundraiser3= "GHI"
+
+fundraiser1_amount = 1000
+fundraiser2_amount = 200
+fundraiser3_amount = 0
+
+puts "#{fundraiser1} has $#{1000} raised so far!"
+
+puts "\nProjects:\n\t#{fundraiser1}\n\t#{fundraiser2}\n\t#{fundraiser3}"
