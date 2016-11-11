0b3ad81e99e05cfd25a42023b2e8512c2c31a730
Code-Dude <smythe3000>
2016-11-10 19:35:07 -0800

    lesson 6 commit

--- fundraiser/fundraiser.rb
+++ fundraiser/fundraiser.rb
@@ -1,11 +1,3 @@
-fundraiser1 = "ABC"
-fundraiser2 = "DEF"
-fundraiser3= "GHI"
-
-fundraiser1_amount = 1000
-fundraiser2_amount = 200
-fundraiser3_amount = 0
-
-puts "#{fundraiser1.ljust(10,"*")}$#{fundraiser1_amount}"
-puts "#{fundraiser2.ljust(10,"*")}$#{fundraiser2_amount}"
-puts "#{fundraiser3.ljust(10,"*")}$#{fundraiser3_amount}"
+def report_funding(project_name,project_funds=0)
+  "#{project_name} has $#{project_funds} in funding currently!"
+end
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,16 +1,12 @@
-player1 = 'larry'
-player2 = 'curly'
-player3 = 'moe'
-player4 = 'shemp'
+def time
+  Time.new.strftime("%I:%M:%S")
+end
 
-player1_health = 60
-player2_health = 120
-player3_health = 100
-player4_health = 90
+def say_hello(name,health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+end
 
-puts "#{player1.capitalize} has a health of #{player1_health}."
-puts "#{player2.upcase} has a health of #{player2_health}."
-puts "#{player3.capitalize} has a health of #{player3_health}".center(50,"*")
-puts "#{player4.capitalize.ljust(20,".")}#{player4_health} health."
-
-puts Time.new.strftime("The game started on %A %m/%d/%Y at %I:%M")
+puts say_hello('larry',60)
+puts say_hello('curly',125)
+puts say_hello('moe')
+puts say_hello('larry',90)
